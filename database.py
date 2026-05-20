1eb56428fdb01c61cb372edcc0a113805e8b5a07
KhangNguyen298 <gongon2926@gmail.com>
2026-05-21 00:28:38 +0700

    connect to DB

--- database.py
+++ database.py
@@ -2,12 +2,12 @@ import os
 import psycopg2
 from dotenv import load_dotenv
 
-# 1. Nạp các biến môi trường từ file .env vào bộ nhớ RAM
+
 load_dotenv()
 
 def test_db_connection():
     try:
-        # 2. Thiết lập đường truyền kết nối xuống PostgreSQL bằng Socket TCP/IP
+        
         connection = psycopg2.connect(
             host=os.getenv("DB_HOST"),
             port=os.getenv("DB_PORT"),
@@ -16,28 +16,29 @@ def test_db_connection():
             password=os.getenv("DB_PASSWORD")
         )
         
-        # 3. Tạo một con trỏ (Cursor) để ra lệnh cho DB
+      
         cursor = connection.cursor()
         
-        # 4. Chạy thử một câu lệnh SQL hỏi phiên bản hệ thống
+       
         cursor.execute("SELECT version();")
         
-        # 5. Hốt hàng dữ liệu đầu tiên trả về từ DB
+
         db_version = cursor.fetchone()
         
         print("\n==========================================")
-        print("🎉 KẾT NỐI POSTGRESQL THÀNH CÔNG RỰC RỠ!")
-        print(f"🤖 Phiên bản DB: {db_version[0]}")
+        print("connect completely")
+        print(f"DB version: {db_version[0]}")
         print("==========================================\n")
         
-        # 6. Đóng kết nối giải phóng tài nguyên bộ nhớ RAM
+       
         cursor.close()
         connection.close()
         
     except Exception as error:
         print("\n==========================================")
-        print(f"❌ LỖI KẾT NỐI DATABASE RỒI BẠN ƠI: {error}")
+        print(f"connect error: {error}")
         print("==========================================\n")
+
 
 if __name__ == "__main__":
     test_db_connection()
